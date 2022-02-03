1bd17ca60f72177345b4573658edc2c9c371d2ed
GuillaumeDuti <dutilleuxguillaume@gmail.com>
2022-02-03 12:24:15 +0100

    title setup and static folder setup

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,10 +1,11 @@
 <script>
-    import logo_bold from "../assets/img/logo_bold.svg"
+    // import logo_bold from "../assets/img/logo_bold.svg"
 </script>
 
 <!-- <nav class="fixed inset-0 w-full py-8 flex justify-center items-center mb-4"> -->
 <nav class="w-full py-8 flex justify-center items-center mb-4">
     <div>
-        <img width="300" height="100" src={logo_bold} alt="Jampickers logo">
+        <img width="300" height="100" src="img/logo_bold.svg" alt="Jampickers logo">
+        <!-- <img width="300" height="100" src={logo_bold} alt="Jampickers logo"> -->
     </div>
 </nav>
