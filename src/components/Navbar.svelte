8c65deb48f0101df013a03c7d8f0e466383ab0ef
GuillaumeDuti <dutilleuxguillaume@gmail.com>
2022-02-03 14:43:14 +0100

    picture and logo sources

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,10 @@
 <!-- <nav class="fixed inset-0 w-full py-8 flex justify-center items-center mb-4"> -->
 <nav class="w-full py-8 flex justify-center items-center mb-4">
     <div>
-        <img width="300" height="100" src="img/logo.png" alt="Jampickers logo">
+        <picture>
+            <source srcset="img/logo_small.png"
+            media="(max-width: 600px)" alt="Jampickers logo">
+            <img class="sm:w-2/4 w-3/5 mx-auto" src="img/logo.png" alt="Jampickers logo">
+        </picture>
     </div>
 </nav>
