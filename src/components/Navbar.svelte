833328562bc2eecb67b3694b4bbdc639ca753757
GuillaumeDuti <dutilleuxguillaume@gmail.com>
2022-02-03 12:46:55 +0100

    update link logo

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,11 +1,6 @@
-<script>
-    // import logo_bold from "../assets/img/logo_bold.svg"
-</script>
-
 <!-- <nav class="fixed inset-0 w-full py-8 flex justify-center items-center mb-4"> -->
 <nav class="w-full py-8 flex justify-center items-center mb-4">
     <div>
         <img width="300" height="100" src="img/logo.png" alt="Jampickers logo">
-        <!-- <img width="300" height="100" src={logo_bold} alt="Jampickers logo"> -->
     </div>
 </nav>
